9692b6bf1b72a75e1229a11b39172ae15e720dac
abdijalilimam <abdijalilimam@gmail.com>
2026-07-13 17:13:21 -0400

    phase 6 - OIDC provider and IAM role for GitHub Actions

--- aws/main.tf
+++ aws/main.tf
@@ -33,3 +33,39 @@ memory = var.memory
 target_group_arn = module.alb.target_group_arn
 alb_security_group_id = module.alb.alb_security_group_id
 }
+
+# OIDC Provider - allows GitHub Actions to authenticate with AWS
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+# IAM Role - grants GitHub Actions temporary access to AWS resources
+resource "aws_iam_role" "github_actions" {
+  name = "github-actions-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.github.arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringLike = {
+          "token.actions.githubusercontent.com:sub" = "repo:abdijalilimam/it-tools:*"
+        }
+      }
+    }]
+  })
+}
+
+#The role needs permission for these 2 actions:
+resource "aws_iam_role_policy_attachment" "github_actions_ecr" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
+}
+resource "aws_iam_role_policy_attachment" "github_actions_admin" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
