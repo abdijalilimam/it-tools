5054b7aae94439d31373151fe0cfb82563a655d3
abdijalilimam <abdijalilimam@gmail.com>
2026-07-12 20:52:33 -0400

    fix: add force_delete to ECR repository

--- aws/modules/ecr/main.tf
+++ aws/modules/ecr/main.tf
@@ -1,9 +1,11 @@
 #added immutable so that the image stay the same.
+#force deletion 
 resource "aws_ecr_repository" "main" {
   name                 = var.repository_name
   image_tag_mutability = "IMMUTABLE"
+  force_delete         = true
 
-tags = {
-Name = var.repository_name
-}
+  tags = {
+    Name = var.repository_name
+  }
 }
